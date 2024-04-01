415d48474b8f0f01a68daaab7dde0a7178e66696
Axel <axelolmar@gmail.com>
2024-04-01 17:37:41 -0600

    Api de token

---
+++ src/ScriptDebugges.AgroDirecta.Api/Controllers/TokenController.cs
@@ -0,0 +1,16 @@
+﻿using ScriptDebugges.AgroDirecta.Api.Models;
+using ScriptDebugges.AgroDirecta.Api.Services;
+using System.Web.Http;
+
+namespace ScriptDebugges.AgroDirecta.Api.Controllers
+{
+    public class TokenController : ApiController
+    {
+        [Route("api/GetToken")]
+        [HttpGet]
+        public TokenResponseModel GenerateNewToken(Guid ID_App)
+        {
+            return new TokenResponseModel() { Token = Security.GenerateNewToken(ID_App) };
+        }
+    }
+}
