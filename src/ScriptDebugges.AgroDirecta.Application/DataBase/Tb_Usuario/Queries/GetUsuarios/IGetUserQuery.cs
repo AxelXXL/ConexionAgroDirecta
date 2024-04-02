08affbb7539d0e5b09cc9629f6fffe98d136fb78
Axel <axelolmar@gmail.com>
2024-04-02 11:39:46 -0600

    Carpeta de Servicios en aplicattion y Capeta para traer usuarios

---
+++ src/ScriptDebugges.AgroDirecta.Application/DataBase/Tb_Usuario/Queries/GetUsuarios/IGetUserQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptDebugges.AgroDirecta.Application.DataBase.Tb_Usuario.Queries.GetUsuarios
+{
+    public interface IGetUserQuery
+    {
+        Task<LoginValidateResponseModel> Execute(LoginValidateRequestModel user);
+    }
+}
---
+++ src/ScriptDebugges.AgroDirecta.Application/Services/Security.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptDebugges.AgroDirecta.Application.Services
+{
+    public class Security
+    {
+        #region Encrypt
+
+        public static string Encrypt(string clearText, string key = "abc123")
+        {
+            string encryptionKey = key;
+            byte[] clearBytes = Encoding.UTF8.GetBytes(clearText);
+
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(encryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                    }
+                    return Convert.ToBase64String(ms.ToArray());
+                }
+            }
+        }
+
+        public static string Decrypt(string cipherText, string key = "abc123")
+        {
+            string encryptionKey = key;
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(encryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                    }
+                    return Encoding.UTF8.GetString(ms.ToArray());
+                }
+            }
+        }
+        #endregion
+    }
+}
