5a4376af083522c3b09d9c3e133706a9751f96b4
davdick <dasddj2@gmail.com>
2024-04-02 13:03:27 -0600

    unidad medida arreglo

--- src/ScriptDebugges.AgroDirecta.Application/DataBase/Tb_UnidadMedida/Commands/Create/CreateUnidadMedidaCommand.cs
+++ src/ScriptDebugges.AgroDirecta.Application/DataBase/Tb_UnidadMedida/Commands/Create/CreateUnidadMedidaCommand.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using ScriptDebugges.AgroDirecta.Application.DataBase.Tb_Productos.Commands.CreateProductos;
 using ScriptDebugges.AgroDirecta.Domain.Entities.Tb_Producto;
+using ScriptDebugges.AgroDirecta.Domain.Entities.Tb_UnidadMedida;
 
 namespace ScriptDebugges.AgroDirecta.Application.DataBase.Tb_UnidadMedida.Commands.Create
 {
@@ -16,8 +17,8 @@ namespace ScriptDebugges.AgroDirecta.Application.DataBase.Tb_UnidadMedida.Comman
 
         public async Task<CreateUnidadMedidaModel> Execute(CreateUnidadMedidaModel model)
         {
-            var entity = _mapper.Map<Tb_UnidadMedida>(model);
-            await _dataBaseService.Tb_Producto.AddAsync(entity);
+            var entity = _mapper.Map<Tb_UnidadMedidaEntity>(model);
+            await _dataBaseService.Tb_UnidadMedida.AddAsync(entity);
             await _dataBaseService.SaveAsync();
             return model;
         }
